bb9f55c7e4f58b0834cc6e2a37ea3f686a8e7772
Leandro Diaz <leandrodiaz042@gmail.com>
2024-01-23 16:38:47 -0300

    make code changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -15,11 +15,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-
-       <h1>Congratulations!</h1>
-  <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
-
+        <h1>Congratulations!</h1>
+        <p>
+          You{"'"}ve completed the essentials of automated application deployment with GitHub
+          Actions!🥳
+        </p>
       </main>
     </>
-  )
+  );
 }
